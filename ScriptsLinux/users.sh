dd7ff6111c5f8ef1a30092a23d5b89df546bcff2
Jesus Paz <jeherpabo@gmail.com>
2019-05-28 19:08:09 -0500

    agrega calcular tamaño al script

--- ScriptsLinux/users.sh
+++ ScriptsLinux/users.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-awk -F: '{print " Nombre: "$5 "\n Home:" $6  "\n Shell:" $7  "\n Espacio Ocupado:  \n"}' /etc/passwd | txt2html > users.html
+awk -F: '{print " Nombre: "$1 "\n Home:" $6  "\n Shell:" $7  "\n Espacio Ocupado: ";system("du -sh "$6" 2> /dev/null")}' /etc/passwd |txt2html > users.html
+
